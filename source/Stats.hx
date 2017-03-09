7b3b9a0ee5841ec0434308d33451238c28fa65ce
cameron taylor <cameron.taylor.ninja@gmail.com>
2017-03-09 14:24:15 -0800

    Some basic skills

--- source/Stats.hx
+++ source/Stats.hx
@@ -6,7 +6,11 @@ package source;
  */
 class Stats 
 {
-
+	public var _animationSkill:Int;
+	public var _artSkill:Int;
+	public var _musicSkill:Int;
+	public var _programSkill:Int; 
+	
 	public function new() 
 	{
 		
