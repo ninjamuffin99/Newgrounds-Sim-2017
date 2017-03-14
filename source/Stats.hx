7aa230c1e093700e5d0536a9b633a158b1370cfe
cameron taylor <cameron.taylor.ninja@gmail.com>
2017-03-14 09:41:54 -0700

    Added date and time variables

--- source/Stats.hx
+++ source/Stats.hx
@@ -10,6 +10,13 @@ class Stats
 	public var _artSkill:Int;
 	public var _musicSkill:Int;
 	public var _programSkill:Int; 
+	public var _sleep:Int;
+	
+	public var yyyy:Int;
+	public var mm:Int;
+	public var dd:Int;
+	public var h:Int;
+	public var m:Int;
 	
 	public function new() 
 	{
