9e79552fcfde5be38be95b54a815a8287044f30d
Cameron <cameron.taylor.ninja@gmail.com>
2017-03-31 21:08:33 -0700

    added store

--- source/StoreState.hx
+++ source/StoreState.hx
@@ -1,6 +1,9 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxState;
+import flixel.addons.ui.FlxButtonPlus;
+import source.Stats;
 
 /**
  * ...
@@ -8,7 +11,32 @@ import flixel.FlxState;
  */
 class StoreState extends FlxState 
 {
-
+	private var _hud:HUD;
+	
+	private var _btnBuyFlash:FlxButtonPlus;
+	
+	override public function create():Void 
+	{
+		_hud = new HUD();
+		add(_hud);
+		
+		createButtons();
+		
+		super.create();
+	}
+	
+	private function createButtons():Void
+	{
+		_btnBuyFlash = new FlxButtonPlus(20, 20, buyFlash, "Buy Flash CS6 $699");
+		add(_btnBuyFlash);
+	}
+	
+	private function buyFlash():Void
+	{
+		Stats._hasFlash = true;
+		FlxG.log.add(_hasFlash);
+	}
+	
 	public function new() 
 	{
 		
